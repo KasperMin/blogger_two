008f0365aca84f01dc672e5addd584d3e2b4b85c
KasperMin <kaspermsimonsen@gmail.comgit config --global user.email git config --global user.email kaspermsimonsen@gmail.com>
2017-09-28 17:29:34 +0200

    finished I2: Adding Comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,19 @@
 class ArticlesController < ApplicationController
 	include ArticlesHelper
 	
+	
 	def index
   	  @articles = Article.all
 	end
 
 	def show
 	  @article = Article.find(params[:id])
+
+	  @comment = Comment.new
+	  @comment.article_id = @article.id
 	end
 
-	# New/create: display empty form -> process data changes
+	# New/create: display empty form -> process data manipulations
 
 	def new
 	  @article = Article.new
@@ -22,7 +26,7 @@ class ArticlesController < ApplicationController
        redirect_to article_path(@article)
 	end
 
-	# Destroy (fabricate delete verb): find article -> process data changes 
+	# Destroy (fabricate delete verb): find article -> process data manipulations 
 
 	def destroy
 	  @article = Article.find(params[:id])
@@ -31,7 +35,7 @@ class ArticlesController < ApplicationController
 	  redirect_to articles_path
 	end
 
-	# Edit/update: find and display form -> process data changes
+	# Edit/update: find and display form -> process data manipulations
 
 	def edit
   	  @article = Article.find(params[:id])
