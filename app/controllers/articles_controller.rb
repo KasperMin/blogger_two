283a6d97f1fd6631c4e9de689670089a32622a2b
KasperMin <kaspermsimonsen@gmail.comgit config --global user.email git config --global user.email kaspermsimonsen@gmail.com>
2017-09-28 11:17:48 +0200

    I1 chapter completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	
 	def index
   	  @articles = Article.all
@@ -6,5 +7,42 @@ class ArticlesController < ApplicationController
 
 	def show
 	  @article = Article.find(params[:id])
+	end
+
+	# New/create: display empty form -> process data changes
+
+	def new
+	  @article = Article.new
+	end
+
+	def create
+ 	   @article = Article.new(article_params)
+ 	   @article.save
+
+       redirect_to article_path(@article)
+	end
+
+	# Destroy (fabricate delete verb): find article -> process data changes 
+
+	def destroy
+	  @article = Article.find(params[:id])
+	  @article.destroy
+
+	  redirect_to articles_path
+	end
+
+	# Edit/update: find and display form -> process data changes
+
+	def edit
+  	  @article = Article.find(params[:id])
+	end
+
+	def update
+  	  @article = Article.find(params[:id])
+ 	  @article.update(article_params)
+
+      flash.notice = "Article '#{@article.title}' Updated!"
+
+   	  redirect_to article_path(@article)
 	end
 end
